be1950c4e285a4ddb65303f8d5724cae8228014a
Jason Keene <jasonkeene@gmail.com>
2014-07-07 21:19:37 -0400

    Stub out Unit Converter.

---
+++ bitfountain-ios7/Unit Converter/Unit Converter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Unit Converter
+//
+//  Created by bonobo on 7/7/14.
+//  Copyright (c) 2014 bonobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bitfountain-ios7/Unit Converter/Unit Converter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Unit Converter
+//
+//  Created by bonobo on 7/7/14.
+//  Copyright (c) 2014 bonobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Testing out breakpoints.");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
