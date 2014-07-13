c5dec19ebb6bdcf297b3c6a98a6e739273c06b94
Jason Keene <jasonkeene@gmail.com>
2014-07-12 21:41:30 -0400

    Add Dog class

---
+++ bitfountain-ios7/Man's Best Friend/Man's Best Friend/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  Man's Best Friend
+//
+//  Created by bonobo on 7/12/14.
+//  Copyright (c) 2014 bonobo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ bitfountain-ios7/Man's Best Friend/Man's Best Friend/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  Man's Best Friend
+//
+//  Created by bonobo on 7/12/14.
+//  Copyright (c) 2014 bonobo. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
