fbd23f5b5474d17e0033ce44a5b137d0b89239ba
Jason Keene <jasonkeene@gmail.com>
2014-07-20 22:00:00 -0400

    Init Pirate Adventure project.

---
+++ bitfountain-ios7/Pirate Adventure/Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure
+//
+//  Created by bonobo on 7/20/14.
+//  Copyright (c) 2014 bonobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bitfountain-ios7/Pirate Adventure/Pirate Adventure/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Pirate Adventure
+//
+//  Created by bonobo on 7/20/14.
+//  Copyright (c) 2014 bonobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
