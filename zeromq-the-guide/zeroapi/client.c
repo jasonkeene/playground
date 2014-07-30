0d5b8428424dc5c4d599a9b55237fc0a52d2c6e7
Jason Keene <jasonkeene@gmail.com>
2014-07-30 17:36:34 -0400

    Stub out client and server.

---
+++ zeromq-the-guide/zeroapi/client.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include <zmq.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("Starting client.\n");
+    printf("Stopping client.\n");
+    return 0;
+}
---
+++ zeromq-the-guide/zeroapi/server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include <zmq.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("Starting server.\n");
+    printf("Stopping server.\n");
+    return 0;
+}
