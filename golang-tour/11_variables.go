e0b8768b0607a84de7b5372a8a64a05a63a1b0aa
Jason Keene <jasonkeene@gmail.com>
2014-08-23 15:58:26 -0400

    Golang tour stuff.

---
+++ golang-tour/11_variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var i, j = 1, 2
+var c, python, java = true, false, "no!"
+
+func main() {
+    fmt.Println(i, j, c, python, java)
+}
---
+++ golang-tour/13_short_variable_declarations.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    i, j, k := 1, 2, 3
+    c, python, java := true, false, "no!"
+    fmt.Println(i, j, k, c, python, java)
+}
