0a4e9c7771eabac6849113806434bcf6634b9d19
Jason Keene <jasonkeene@gmail.com>
2014-11-17 16:35:47 -0500

    Factor out minion install script.

--- saltstack/prototypes/bin/bootstrap-master.sh
+++ saltstack/prototypes/bin/bootstrap-master.sh
@@ -14,14 +14,7 @@ apt-get install --yes python-dev
 apt-get install --yes python-pip
 
 # install salt minion
-add-apt-repository ppa:saltstack/salt
-echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | tee /etc/apt/sources.list.d/saltstack.list
-wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | apt-key add -
-apt-get update
-apt-get install --yes salt-minion
-
-# stop miniond
-service salt-minion stop
+/vagrant/bin/install-minion.sh
 
 # link in salt states
 ln -s /vagrant/salt /srv/salt
