b4ca0646a2ec752162d771d5839d6ec74ba6d775
Jason Keene <jasonkeene@gmail.com>
2015-07-28 17:43:23 -0400

    That was simple.

--- concurrent-prime-sieve/sieve.go
+++ concurrent-prime-sieve/sieve.go
@@ -9,13 +9,33 @@ import (
 var n = flag.Int("n", 1, "count of how many primes to calculate")
 
 func Primes() chan int {
-	prime_chan := make(chan int)
+	out := make(chan int)
+	in := NewSieve(out)
 	go func() {
-		for {
-			prime_chan <- 2
+		for i := 2; ; i++ {
+			in <- i
 		}
 	}()
-	return prime_chan
+	return out
+}
+
+func NewSieve(out chan int) chan int {
+	in := make(chan int)
+	go sieve(in, out)
+	return in
+}
+
+func sieve(in, out chan int) {
+	prime := <-in
+	out <- prime
+
+	child_in := NewSieve(out)
+	for {
+		possible_prime := <-in
+		if possible_prime%prime != 0 {
+			child_in <- possible_prime
+		}
+	}
 }
 
 func main() {
