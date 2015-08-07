1138d6f59f950d60ce6ee5370255c63ad11dc5b3
Zach Kanzler and Jason Keene <they4kman@gmail.com>
2015-08-06 21:09:07 -0400

    Flesh out hash table implementation.

--- data-structures/hash-table/hash.go
+++ data-structures/hash-table/hash.go
@@ -1,5 +1,70 @@
 package hash
 
-func Hash(data []byte) int {
-	return 1
+import (
+	"crypto/sha1"
+	"encoding/binary"
+)
+
+// Design:
+// - Table size should be power of two
+// - Use only builtin arrays (maybe slices)
+// - Use bit masking to mod into table size
+// - Table should dynamically resize to next power when collisions threshold
+//   is reached
+// - Keys will need to be stored in buckets with values for verification
+//   checking and to allow for rehashing
+// - Will not use open addressing
+
+const InitialCapacity = 8
+
+type BucketValue struct {
+	key   string
+	value interface{}
 }
+
+// TODO: make buckets contain multiple bucket values for collisions
+type Bucket BucketValue
+
+type Hash struct {
+	buckets []Bucket
+}
+
+func NewHash() *Hash {
+	return &Hash{
+		buckets: make([]Bucket, InitialCapacity),
+	}
+}
+
+func (h *Hash) Set(k string, v interface{}) {
+	index := truncate(HashFunc([]byte(k)), len(h.buckets))
+	h.buckets[index] = Bucket{
+		key:   k,
+		value: v,
+	}
+}
+
+func (h *Hash) Get(k string) interface{} {
+	index := truncate(HashFunc([]byte(k)), len(h.buckets))
+	// TODO: verify key to detect collisions
+	return h.buckets[index].value
+}
+
+func HashFunc(data []byte) uint64 {
+	hash := sha1.New()
+	hash.Write(data)
+	sum := make([]byte, 0)
+	sum = hash.Sum(sum)
+
+	result := binary.BigEndian.Uint64(sum)
+	return result
+}
+
+func truncate(value uint64, size int) uint64 {
+	return value % uint64(size)
+}
+
+// TODO: use masking vs mod
+
+// 000000000000 1111
+// 101010101101 0101  bitwise and
+// 000000000000 0101
--- data-structures/hash-table/hash_test.go
+++ data-structures/hash-table/hash_test.go
@@ -1,16 +1,49 @@
 package hash
 
 import (
+	"strconv"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func TestHash(t *testing.T) {
+	Convey("given an empty hash", t, func() {
+		hash := NewHash()
+		Convey("it adds elements", func() {
+			hash.Set("key", "value")
+		})
+		Convey("it remembers elements", func() {
+			hash.Set("key", "value")
+			So(hash.Get("key"), ShouldEqual, "value")
+		})
+		Convey("it remembers multiple values", func() {
+			hash.Set("key", "value")
+			hash.Set("key2", "value2")
+			So(hash.Get("key"), ShouldEqual, "value")
+			So(hash.Get("key2"), ShouldEqual, "value2")
+		})
+	})
+}
+
 func TestHashFunction(t *testing.T) {
 	Convey("given a slice of bites", t, func() {
 		data := []byte("hello hash")
 		Convey("it hashes into an integer", func() {
-			So(Hash(data), ShouldHaveSameTypeAs, 1)
+			So(HashFunc(data), ShouldHaveSameTypeAs, uint64(1))
+		})
+	})
+	Convey("given a sample of hash results", t, func() {
+		sample_size := 1000
+		sample := make(map[uint64]bool)
+		for i := 0; i < sample_size; i++ {
+			sample[HashFunc([]byte(strconv.Itoa(i)))] = true
+		}
+		Convey("it hashes into different values for different inputs", func() {
+			So(len(sample), ShouldEqual, sample_size)
+		})
+		Convey("it produces a uniform distribution", func() {
+			// TODO: look into chi square test
 		})
 	})
 }
@@ -19,6 +52,6 @@ func BenchmarkHashFunction(b *testing.B) {
 	data := []byte("hello hash")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Hash(data)
+		HashFunc(data)
 	}
 }
