2eed97f2ff25a55946fc148ff74428c7244435c2
Jason Keene <jasonkeene@gmail.com>
2019-01-06 14:11:38 -0700

    Fix bug with insertion sort

--- algorithms/sort/insertion.go
+++ algorithms/sort/insertion.go
@@ -3,12 +3,13 @@ package sort
 func Insertion(a []int) {
 	for i := 1; i < len(a); i++ {
 		key := a[i]
-		for j := i - 1; j >= 0; j-- {
+		var j int
+		for j = i - 1; j >= 0; j-- {
 			if a[j] <= key {
-				a[j+1] = key
 				break
 			}
 			a[j+1] = a[j]
 		}
+		a[j+1] = key
 	}
 }
--- algorithms/sort/sort_test.go
+++ algorithms/sort/sort_test.go
@@ -3,6 +3,8 @@ package sort_test
 import (
 	"math"
 	"math/rand"
+	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -10,12 +12,13 @@ type sortingFunc func([]int)
 
 func testCorrectness(t *testing.T, f sortingFunc) {
 	cases := map[string][]int{
-		"empty":           []int{},
-		"single item":     []int{5},
-		"multiple items":  []int{5, 8, 16, 2, 71, -13, 61},
-		"duplicate items": []int{5, 5, 5, 2, 71, -13, 61},
-		"already sorted":  []int{-13, 2, 5, 8, 16, 61, 71},
-		"reversed sorted": []int{71, 61, 16, 8, 5, 2, -13},
+		"empty":              []int{},
+		"single item":        []int{5},
+		"multiple items":     []int{5, 8, 16, 2, 71, -13, 61},
+		"duplicate items":    []int{5, 5, 5, 2, 71, -13, 61},
+		"all the same items": []int{5, 5, 5, 5, 5, 5, 5},
+		"already sorted":     []int{-13, 2, 5, 8, 16, 61, 71},
+		"reversed sorted":    []int{71, 61, 16, 8, 5, 2, -13},
 
 		"extra_small_rand_8": random(4, 8),
 		"small_rand_8":       random(64, 8),
@@ -35,55 +38,19 @@ func testCorrectness(t *testing.T, f sortingFunc) {
 
 	for k, v := range cases {
 		t.Run(k, func(t *testing.T) {
+			expected := make([]int, len(v))
+			copy(expected, v)
+			sort.Ints(expected)
+
 			f(v)
-			problem, ok := sorted(v)
-			if !ok {
+
+			if !reflect.DeepEqual(expected, v) {
 				t.Errorf(`%q failed to sort:
-	Array State: %v
-	Problem:
-		Index: %d
-		Area: %v`, k, v, problem.index, problem.area)
+	Expected: %v
+	Actual: %v`, k, expected, v)
 			}
 		})
 	}
-}
-
-type problem struct {
-	index int
-	area  []int
-}
-
-func sorted(a []int) (problem, bool) {
-	var last int
-	for i, v := range a {
-		if i == 0 {
-			last = v
-			continue
-		}
-
-		if v < last {
-			return problem{
-				index: i,
-				area:  area(a, i),
-			}, false
-		}
-	}
-
-	return problem{}, true
-}
-
-func area(a []int, i int) []int {
-	h := i - 1
-	j := i + 1
-
-	if h < 0 {
-		h = i
-	}
-	if j >= len(a) {
-		j = i
-	}
-
-	return a[h : j+1]
 }
 
 func random(n, s int) []int {
