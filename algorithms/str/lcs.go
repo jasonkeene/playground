6e2c0908113dc099059a2011471c892b627f9cb4
Jason Keene <jasonkeene@gmail.com>
2019-02-16 11:21:02 -0700

    Implement LCS table solution

--- algorithms/str/lcs.go
+++ algorithms/str/lcs.go
@@ -1,36 +1,135 @@
 package str
 
-import "fmt"
-
-var cache = make(map[string]string)
-
 func LCS(a, b string) string {
-	result, ok := cache[key(a, b)]
+	cache := make(map[string]string)
+	return lcs(a, b, cache)
+}
+
+func lcs(a, b string, cache map[string]string) string {
+	v, ok := cache[key(a, b)]
 	if ok {
-		return result
+		return v
 	}
 
-	fmt.Printf("solving: %q %q\n", a, b)
 	if len(a) == 0 || len(b) == 0 {
 		cache[key(a, b)] = ""
 		return ""
 	}
 
 	if a[len(a)-1] == b[len(b)-1] {
-		result = LCS(a[:len(a)-1], b[:len(b)-1]) + string(b[len(b)-1])
-		cache[key(a, b)] = result
-		return result
+		v = lcs(a[:len(a)-1], b[:len(b)-1], cache) + string(b[len(b)-1])
+		cache[key(a, b)] = v
+		return v
 	}
-	alcs := LCS(a[:len(a)-1], b)
-	blcs := LCS(a, b[:len(b)-1])
-	if alcs > blcs {
-		cache[key(a, b)] = alcs
-		return alcs
+	av := lcs(a[:len(a)-1], b, cache)
+	bv := lcs(a, b[:len(b)-1], cache)
+	if av > bv {
+		cache[key(a, b)] = av
+		return av
 	}
-	cache[key(a, b)] = blcs
-	return blcs
+	cache[key(a, b)] = bv
+	return bv
 }
 
 func key(a, b string) string {
 	return a + "|" + b
+}
+
+func LCSTable(a, b string) string {
+	table := LCSLengthTable(a, b)
+	return assembleLCS(a, b, table, len(a)-1, len(b)-1)
+}
+
+func assembleLCS(a, b string, table [][]int, i, j int) string {
+	if value(table, i, j) == 0 {
+		return ""
+	}
+
+	if a[i] == b[j] {
+		return assembleLCS(a, b, table, i-1, j-1) + string(a[i])
+	}
+
+	if above(table, i, j) > left(table, i, j) {
+		return assembleLCS(a, b, table, i-1, j)
+	}
+
+	return assembleLCS(a, b, table, i, j-1)
+}
+
+func AllLCS(a, b string) map[string]struct{} {
+	results := make(map[string]struct{})
+	table := LCSLengthTable(a, b)
+	assembleAllLCS(a, b, "", table, results, len(a)-1, len(b)-1)
+	return results
+}
+
+func assembleAllLCS(a, b, partial string, table [][]int, results map[string]struct{}, i, j int) {
+	if value(table, i, j) == 0 {
+		results[partial] = struct{}{}
+		return
+	}
+
+	if a[i] == b[j] {
+		assembleAllLCS(a, b, string(a[i])+partial, table, results, i-1, j-1)
+		return
+	}
+
+	u := above(table, i, j)
+	l := left(table, i, j)
+
+	if u > l {
+		assembleAllLCS(a, b, partial, table, results, i-1, j)
+		return
+	}
+
+	if u < l {
+		assembleAllLCS(a, b, partial, table, results, i, j-1)
+		return
+	}
+
+	assembleAllLCS(a, b, partial, table, results, i-1, j)
+	assembleAllLCS(a, b, partial, table, results, i, j-1)
+}
+
+func LCSLengthTable(a, b string) [][]int {
+	table := make([][]int, len(a))
+	for i := range table {
+		table[i] = make([]int, len(b))
+
+		for j := range table[i] {
+			if a[i] == b[j] {
+				table[i][j] = diagonal(table, i, j) + 1
+				continue
+			}
+
+			table[i][j] = max(above(table, i, j), left(table, i, j))
+		}
+	}
+	return table
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func value(table [][]int, i, j int) int {
+	if i < 0 || j < 0 {
+		return 0
+	}
+	return table[i][j]
+}
+
+func diagonal(table [][]int, i, j int) int {
+	return value(table, i-1, j-1)
+}
+
+func above(table [][]int, i, j int) int {
+	return value(table, i-1, j)
+}
+
+func left(table [][]int, i, j int) int {
+	return value(table, i, j-1)
 }
--- algorithms/str/lcs_test.go
+++ algorithms/str/lcs_test.go
@@ -3,6 +3,7 @@ package str_test
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/jasonkeene/playground/algorithms/str"
 )
 
@@ -55,12 +56,48 @@ func TestLCS(t *testing.T) {
 		},
 	}
 
+	impls := map[string](func(string, string) string){
+		"LCS":      str.LCS,
+		"LCSTable": str.LCSTable,
+	}
+
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			result := str.LCS(tc.a, tc.b)
-			if _, ok := tc.lcs[result]; !ok {
-				t.Fatalf("invalid result: %q", result)
+			for fname, f := range impls {
+				t.Run(fname, func(t *testing.T) {
+					result := f(tc.a, tc.b)
+					if _, ok := tc.lcs[result]; !ok {
+						t.Fatalf("invalid result: %q", result)
+					}
+				})
 			}
 		})
+
+		t.Run("all "+name, func(t *testing.T) {
+			result := str.AllLCS(tc.a, tc.b)
+
+			if !cmp.Equal(result, tc.lcs) {
+				t.Fatal(cmp.Diff(result, tc.lcs))
+			}
+		})
+	}
+}
+
+func TestLCSLengthTable(t *testing.T) {
+	a := "CATCGA"
+	b := "GTACCGTCA"
+
+	result := str.LCSLengthTable(a, b)
+	expected := [][]int{
+		{0, 0, 0, 1, 1, 1, 1, 1, 1},
+		{0, 0, 1, 1, 1, 1, 1, 1, 2},
+		{0, 1, 1, 1, 1, 1, 2, 2, 2},
+		{0, 1, 1, 2, 2, 2, 2, 3, 3},
+		{1, 1, 1, 2, 2, 3, 3, 3, 3},
+		{1, 1, 2, 2, 2, 3, 3, 3, 4},
+	}
+
+	if !cmp.Equal(result, expected) {
+		t.Fatal(cmp.Diff(result, expected))
 	}
 }
