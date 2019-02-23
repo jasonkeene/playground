87998d28ff4b811f4e3fe125641dc1ca48da83a2
Jason Keene <jasonkeene@gmail.com>
2019-02-23 15:41:10 -0700

    Clean up data-structures package

--- data-structures/hash-table/hash_test.go
+++ data-structures/hash-table/hash_test.go
@@ -1,57 +1,43 @@
-package hash
+package hash_test
 
 import (
 	"strconv"
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	hash "github.com/jasonkeene/playground/data-structures/hash-table"
 )
 
 func TestHash(t *testing.T) {
-	Convey("given an empty hash", t, func() {
-		hash := NewHash()
-		Convey("it adds elements", func() {
-			hash.Set("key", "value")
-		})
-		Convey("it remembers elements", func() {
-			hash.Set("key", "value")
-			So(hash.Get("key"), ShouldEqual, "value")
-		})
-		Convey("it remembers multiple values", func() {
-			hash.Set("key", "value")
-			hash.Set("key2", "value2")
-			So(hash.Get("key"), ShouldEqual, "value")
-			So(hash.Get("key2"), ShouldEqual, "value2")
-		})
-	})
+	h := hash.NewHash()
+
+	h.Set("key", "value")
+	h.Set("key2", "value2")
+
+	if h.Get("key") != "value" {
+		t.Fatal("key != value")
+	}
+	if h.Get("key2") != "value2" {
+		t.Fatal("key2 != value2")
+	}
 }
 
 func TestHashFunction(t *testing.T) {
-	Convey("given a slice of bites", t, func() {
-		data := []byte("hello hash")
-		Convey("it hashes into an integer", func() {
-			So(HashFunc(data), ShouldHaveSameTypeAs, uint64(1))
-		})
-	})
-	Convey("given a sample of hash results", t, func() {
-		sample_size := 1000
-		sample := make(map[uint64]bool)
-		for i := 0; i < sample_size; i++ {
-			sample[HashFunc([]byte(strconv.Itoa(i)))] = true
-		}
-		Convey("it hashes into different values for different inputs", func() {
-			So(len(sample), ShouldEqual, sample_size)
-		})
-		Convey("it produces a uniform distribution", func() {
-			// TODO: look into chi square test
-		})
-	})
+	const sample_size = 1000
+	sample := make(map[uint64]bool, sample_size)
+	for i := 0; i < sample_size; i++ {
+		sample[hash.HashFunc([]byte(strconv.Itoa(i)))] = true
+	}
+
+	if len(sample) != sample_size {
+		t.Fatalf("hash func did not have uniqe values for each sample")
+	}
+	// TODO: look into chi square test for uniform distribution
 }
 
 func BenchmarkHashFunction(b *testing.B) {
 	data := []byte("hello hash")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		HashFunc(data)
+		hash.HashFunc(data)
 	}
 }
